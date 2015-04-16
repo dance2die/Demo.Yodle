28e903d0ab28d6084201cb2ae9aed53d82960a59
dance2die <dontbothermewithspam@gmail.com>
2015-04-15 21:38:32 -0400

    I don't know if 744494 is the correct value or not. needs more testing. wish I had used TDD for this.

--- Demo.Yodle/Demo.Yodle.Triangle.Console/Program.cs
+++ Demo.Yodle/Demo.Yodle.Triangle.Console/Program.cs
@@ -1,31 +1,65 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Demo.Yodle.Triangle.Console
 {
 	public class Program
 	{
-		private static int NUM_ROWS = 100;
 		private static string INPUT_FILE = @".\triangle.txt";
+		private static string TEST_INPUT_FILE = @".\triangle - test.txt";
 
 		public static void Main(string[] args)
 		{
-			var array = GetInputArray();
+			var array = GetInputArray(INPUT_FILE);
+			//var array = GetInputArray(TEST_INPUT_FILE);
+			int result = FindMaxiumTotal(array);
+			System.Console.WriteLine("Result: {0}", result);
 		}
 
-		public static int[][] GetInputArray()
+		private static int FindMaxiumTotal(int[][] input)
 		{
-			int[][] result = new int[NUM_ROWS][];
-			int row = 0;
-			foreach (var line in File.ReadLines(INPUT_FILE))
+			int result = 0;
+			int middleIndex = 0;
+			for (int rowIndex = 0; rowIndex < input.Length; rowIndex++)
+			{
+				int leftValue = GetInputValue(input, rowIndex, middleIndex - 1);
+				int middleValue = GetInputValue(input, rowIndex, middleIndex);
+				int rightValue = GetInputValue(input, rowIndex, middleIndex + 1);
+				MaxValueInfo maxValueInfo = GetMaxValueInfo(middleIndex, new []{leftValue, middleValue, rightValue});
+				result += maxValueInfo.MaxValue;
+				middleIndex = maxValueInfo.Index;
+			}
+
+			return result;
+		}
+
+		private static MaxValueInfo GetMaxValueInfo(int middleIndex, int[] values)
+		{
+			var maxValue = values.Max();
+			int maxIndex = values.ToList().IndexOf(maxValue);
+			return new MaxValueInfo(middleIndex + maxIndex, maxValue);
+		}
+
+
+		private static int GetInputValue(int[][] input, int rowIndex, int columnIndex)
+		{
+			if (input.Length <= rowIndex || rowIndex < 0) return 0;
+			if (input[rowIndex].Length <= columnIndex || columnIndex < 0) return 0;
+
+			return input[rowIndex][columnIndex];
+		}
+
+		public static int[][] GetInputArray(string inputFile)
+		{
+			int rowIndex = 0;
+			var lines = File.ReadLines(inputFile).ToList();
+			int[][] result = new int[lines.Count][];
+			foreach (var line in lines)
 			{
 				var separator = new[] { ' ' };
 				var numbers = line.Split(separator, StringSplitOptions.RemoveEmptyEntries);
-				result[row] = new int[numbers.Length];
+				result[rowIndex] = new int[numbers.Length];
 
 				int col = 0;
 				foreach (string numberText in numbers)
@@ -34,15 +68,27 @@ namespace Demo.Yodle.Triangle.Console
 					bool isParsed = int.TryParse(numberText, out number);
 					if (!isParsed) throw new Exception("Input file is not correct!");
 
-					result[row][col] = number;
+					result[rowIndex][col] = number;
 					col++;
 				}
 				col = 0;
 
-				row++;
+				rowIndex++;
 			}
 
 			return result;
+		}
+	}
+
+	public class MaxValueInfo
+	{
+		public int Index { get; set; }
+		public int MaxValue { get; set; }
+
+		public MaxValueInfo(int index, int maxValue)
+		{
+			Index = index;
+			MaxValue = maxValue;
 		}
 	}
 }
